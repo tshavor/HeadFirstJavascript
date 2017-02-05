d5d0c4a5a17252bd09fff33fd5139ced15907983
Thomas J. Shavor <thomasshavor@gmail.com>
2017-02-05 12:31:21 -0600

    added battleship game folder

---
+++ sleepy.js
@@ -0,0 +1,12 @@
+var word = "sheep";
+var count = 0;
+
+while (count < 50) {
+console.log(count + " " + word);
+count = count + 1;
+}
+
+if (count === 50){
+console.log("Sleepy time, Little Mario...");
+}
+
